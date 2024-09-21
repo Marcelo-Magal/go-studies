2c8e2ae1ac6dc9793aa337ffbc5cf0b28f40a47c
marcelo-magal <marcelo.magalhaes@ifood.com.br>
2024-09-21 12:07:43 -0300

    Initial commit with project structure

---
+++ basics/go.mod
@@ -0,0 +1,3 @@
+module go-studies/basics
+
+go 1.23.1
---
+++ basics/main.go
@@ -0,0 +1,18 @@
+// O arquivo principal que inicia o programa e chama as funções definidas nos outros arquivos.
+
+package main
+
+import "github.com/Marcelo-Magal/go-studies/basics"
+
+
+
+// #### Explicação:
+
+// - **package main**: 
+//   - Todo programa Go precisa de um pacote principal, que é o `package main`. O pacote `main` define o ponto de entrada do programa, onde a execução começa.
+  
+// - **import "go-studies/basics/greetings"**: 
+//   - A palavra-chave `import` é usada para incluir outros pacotes no seu programa. Aqui, estamos importando o pacote `greetings`, que está no diretório `basics/greetings`, para que possamos usar suas funções dentro do `main.go`.
+  
+// - **func main() { ... }**: 
+//   - Esta é a função principal (`main()`). É a função especial onde o programa começa a rodar. Todo código que for executado inicialmente será colocado dentro desta função.
